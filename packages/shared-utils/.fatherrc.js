0f16444a9eeff71741349edff61b078267d741bd
wucl <wucl@shinemo.com>
2023-12-03 21:40:46 +0800

    chore: 修改father部分文档

--- packages/shared-utils/.fatherrc.js
+++ packages/shared-utils/.fatherrc.js
@@ -1,4 +1,10 @@
 export default {
+  // 以下为 esm 配置项启用时的默认值，有自定义需求时才需配置
+  esm: {
+    input: 'src', // 默认编译目录
+    platform: 'browser', // 默认构建为 Browser 环境的产物
+    transformer: 'babel' // 默认使用 babel 以提供更好的兼容性
+  },
   cjs: {
     input: 'src', // 默认编译目录
     platform: 'node', // 默认构建为 Node.js 环境的产物
